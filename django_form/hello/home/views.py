2fc01a0329b25800900fbb2cdaa7060e6dfb32b2
Tanupriya Pathak <87934620+tanupriya9102@users.noreply.github.com>
2022-07-13 13:58:35 +0530

    Add files via upload

---
+++ django_form/hello/home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request, 'contact.html')
+ 
