85071ccca7253003b866a7630145c9deb6dab768
Tanupriya Pathak <87934620+tanupriya9102@users.noreply.github.com>
2022-07-16 13:49:16 +0530

    Delete textanalyzer directory

--- textanalyzer/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-urlpatterns = [
-   path("",views.index, name='index'),
-   path("index",views.index, name='index'),
-   path("analyze",views.analyze, name='analyze')
-
-]
